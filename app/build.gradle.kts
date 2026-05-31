1b5872a9d9ab8d4c7fdc62755b41cd1e3cff1b61
rsps1008 <rsps1008@gmail.com>
2026-05-31 21:01:01 +0800

    版本1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.rsps1008.stockify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 130
-        versionName = "1.3.0"
+        versionCode = 131
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
