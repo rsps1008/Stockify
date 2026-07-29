01df317a94964e999afc8af6d828ebbc8e22878a
rsps1008 <rsps1008@gmail.com>
2026-07-29 10:19:37 +0800

    版本1.6.0、新增補充保費自動計算

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.rsps1008.stockify"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1591
-        versionName = "1.5.9.1"
+        versionCode = 1600
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
