28f8350fc39447180dbc2013e2a1ba581631c625
rsps1008 <rsps1008@gmail.com>
2026-08-18 10:26:36 +0800

    版本1.6.3-1633

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.rsps1008.stockify"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1632
+        versionCode = 1633
         versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
